05ed8ce38db60427129754d4f1ae83029dc79b17
Prashant Mishra <prashant24.juit@gmail.com>
2023-04-24 14:32:55 -0700

    Replace script processor node with audioworklet for audio recorder

--- dist/typings/audio_player.d.ts
+++ dist/typings/audio_player.d.ts
@@ -1,21 +1,14 @@
 export declare class AudioPlayer {
-    private audioContext;
-    private gainNode;
-    private chunks;
     private sampleRate;
-    private startTime;
-    private lastChunkOffset;
-    private fadeDuration;
-    private minVol;
-    private maxVol;
+    private asyncQueue;
+    private audio;
+    private url;
     private onPlay;
     private onStop;
     private isPlaying;
     constructor(sampleRate: number);
-    private convertLinearPCMToFloat32;
-    private createChunk;
-    private addFadeInFadeOut;
-    addChunk(data: Uint8Array): void;
+    private arrayBufferToBase64;
+    addChunk(data: Uint8Array, sampleRate?: number | null): void;
     onPlayStart(fn: () => void): void;
     onPlayStop(fn: () => void): void;
 }
